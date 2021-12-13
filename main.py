8dad53c210ec37aff6aaf168ac978bb4d0895cb7
Chalsey-D <80493509+Chalsey-D@users.noreply.github.com>
2021-12-13 19:57:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+consumables = ["potion"]
+weapon = ["wooden sword"]
+armor = False
+
+def main():
+  #this is the menu the user will use.
+  print("What would you like to do adventurer?\n")
+  print("1.Check inventory")
+  print("2.Go to Shop")
+  print("3.Go on an Adventure")
+  print("4.Go Home\n")
+
+  choice = int(input())
+  
+
+main()
