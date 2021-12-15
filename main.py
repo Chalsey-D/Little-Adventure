b38567e3c0aa2097304194846fde19442eb4592f
Chalsey Dorce <80493509+Chalsey-D@users.noreply.github.com>
2021-12-15 15:30:20 +0000

    added check inventory and shop functions. defined shop's inventory and player's inventory

--- main.py
+++ main.py
@@ -1,7 +1,47 @@
+# Adventurer's starting inventory
 consumables = ["potion"]
 weapon = ["wooden sword"]
 armor = False
+wallet = 0
 
+#Shop's starting inventory
+shop_consumables = {
+  "poiton": 25,
+  "antidote":50,
+}
+shop_Armor = {
+  "bronze helm":75,
+  "chainmail":300,
+  "leather boots":100
+}
+
+shop_Weapons = {
+  "bronze sword":150,
+  "bow":75,
+  "arrow":100
+}
+
+#this function checks the inventory
+def Check_inventory():
+  print("Consumables = ",consumables)
+  print("Weapon = ",weapon)
+  print("Armor = ",armor)
+  print("Money = ",wallet)
+
+#this function creates the shop
+def Go_to_Shop():
+  print("Hello what are you buying?\n")
+  print("1.Consumables")
+  print("2.Armor")
+  print("3.Weapons")
+
+  answer = int(input())
+
+  if answer == 1:
+    
+
+
+#this is the menu
 def main():
   #this is the menu the user will use.
   print("What would you like to do adventurer?\n")
@@ -13,10 +53,11 @@ def main():
   choice = int(input())
 
   if choice == 1:
-    print("Consumables = ",consumables)
-    print("Weapon = ",weapon)
-    print("Armor = ",armor)
+    Check_inventory()
   
+  elif == 2:
+
+
   else:
     print("We are done here")
 
